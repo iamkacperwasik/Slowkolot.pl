dba228cf1e8f15f5c0bd058d17697771fe483d26
Kacper Wąsik <kacper@wasik.me>
2023-09-03 01:28:55 +0200

    feat(LikeButton): log ip addr on click

--- app/components/Like/LikeButton.tsx
+++ app/components/Like/LikeButton.tsx
@@ -1,8 +1,21 @@
+'use client';
+
+import {IpPostResponse} from 'ip/route';
 import {AiOutlineLike as LikeIcon} from 'react-icons/ai';
 
 export const LikeButton = () => {
+  const updateLikes = async () => {
+    const response = await fetch('/ip', {
+      method: 'POST',
+    });
+
+    const {ip}: IpPostResponse = await response.json();
+
+    console.log(ip);
+  };
+
   return (
-    <button className='cursor-pointer'>
+    <button className='cursor-pointer' onClick={updateLikes}>
       <LikeIcon />
     </button>
   );
---
+++ app/ip/route.ts
@@ -0,0 +1,12 @@
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+
+export type IpPostResponse = {
+  ip: string;
+};
+
+export function POST({ip}: NextRequest): NextResponse<IpPostResponse> {
+  return NextResponse.json({
+    ip: ip ?? '127.0.0.1',
+  });
+}
