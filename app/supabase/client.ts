dec6217761acbbdb53612952539aeef440156282
Kacper Wąsik <kacper@wasik.me>
2023-09-03 05:17:31 +0200

    refactor(supabase/): move helpers to separate file

--- app/supabase/client.ts
+++ app/supabase/client.ts
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js';
 import {Database} from 'supabase/database.types';
 
-export const supabaseClient = createClient<Database>(
+export const supabase_client = createClient<Database>(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY,
   {
--- app/supabase/database.types.ts
+++ app/supabase/database.types.ts
@@ -1,13 +1,3 @@
-import {PostgrestError} from '@supabase/supabase-js';
-
-export type DbResult<T> = T extends PromiseLike<infer U> ? U : never;
-export type DbResultOk<T> = T extends PromiseLike<{data: infer U}> ? Exclude<U, null> : never;
-export type DbResultErr = PostgrestError;
-
-export type Tables<T extends keyof Database['public']['Tables']> =
-  Database['public']['Tables'][T]['Row'];
-export type Enums<T extends keyof Database['public']['Enums']> = Database['public']['Enums'][T];
-
 export type Json = string | number | boolean | null | {[key: string]: Json | undefined} | Json[];
 
 export interface Database {
