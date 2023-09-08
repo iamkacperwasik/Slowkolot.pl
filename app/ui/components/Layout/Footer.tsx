b2bc87f7f9a24ac37825a39e98820a46eebf9c3b
Kacper Wąsik <kacper@wasik.me>
2023-09-09 00:00:38 +0200

    feat: add link to source code in Footer

--- app/ui/components/Layout/Footer.tsx
+++ app/ui/components/Layout/Footer.tsx
@@ -9,21 +9,31 @@ export const Footer = () => {
   // TODO: `fetch words_count`
 
   return (
-    <footer className='my-4 text-sm'>
-      <div className='container mx-auto flex justify-between items-center'>
+    <footer className='my-4 text-sm flex justify-between items-center'>
+      <div className='flex gap-4 items-center'>
         <div>
           <span>&copy; 2023 </span>
-          <Link href='https://kacper.wasik.me' target='_blank'>
+          <Link href='https://kacper.wasik.me' target='_blank' className='hover:underline'>
             Kacper Wąsik
           </Link>
         </div>
 
-        <Link href='/list'>
-          <div>
-            Liczba dostępnych słów: <span className='font-bold'>{words_count}</span>
-          </div>
+        <div className='w-[2px] h-[15px] border-r border-gray-300' />
+
+        <Link
+          href='https://github.com/iamkacperwasik/Slowkolot.pl'
+          target='_blank'
+          className='hover:underline'
+        >
+          Source code
         </Link>
       </div>
+
+      <Link href='/list' className='hover:underline'>
+        <div>
+          Liczba dostępnych słów: <span className='font-bold'>{words_count}</span>
+        </div>
+      </Link>
     </footer>
   );
 };
