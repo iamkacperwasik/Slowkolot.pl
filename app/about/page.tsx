2896e6b016da22fa107c42eeda165a3eae024541
Kacper Wąsik <kacper@wasik.me>
2023-09-09 00:53:44 +0200

    feat: add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,56 @@
+export default function About() {
+  return (
+    <div className='mb-8'>
+      <p className='text-lg'>
+        Słówkolot.pl to minimalistyczna strona internetowa, która zaprasza Cię do fascynującej
+        przygody pośród ciekawostek słowniczych w języku polskim.
+      </p>
+
+      <h2 className='font-bold text-3xl my-8'>Jak to działa?</h2>
+
+      <div className='flex flex-col gap-8 mb-8'>
+        <div>
+          <h2 className='font-bold text-xl mb-4'>Losowe Słowo</h2>
+
+          <p className='text-lg'>
+            Po wejściu na Słówkolot.pl przywitamy Cię losowo wybranym słowem. To słowo może być
+            zupełnie nowe dla Ciebie i otworzyć przed Tobą drzwi do nieznanych znaczeń.
+          </p>
+        </div>
+
+        <div>
+          <h2 className='font-bold text-xl mb-4'>Słowo + Definicja</h2>
+
+          <p className='text-lg'>
+            Każde słowo jest prezentowane wraz z jego klarowną definicją. Dowiesz się, co oznacza to
+            słowo i jakie ma znaczenie.
+          </p>
+        </div>
+
+        <div>
+          <h2 className='font-bold text-xl mb-4'>Rozszerzaj Horyzonty</h2>
+
+          <p className='text-lg '>
+            Słówkolot.pl to miejsce, gdzie możesz poszerzać swoje horyzonty słownicze w języku
+            polskim, odkrywając unikalne słowa i zwroty.
+          </p>
+        </div>
+
+        <div>
+          <h2 className='font-bold text-xl mb-4'>Codzienna Dawka Wiedzy</h2>
+
+          <p className='text-lg'>
+            Wróć do nas codziennie, aby odkrywać nowe słowa i stopniowo wzbogacać swoją wiedzę o
+            języku.
+          </p>
+        </div>
+      </div>
+
+      <p className='text-lg'>
+        Słówkolot.pl to prosty, ale fascynujący projekt, który ukazuje piękno i różnorodność języka
+        polskiego. Poznawaj nowe słowa i zanurzaj się w ciekawostkach słowniczych z Słówkolot.pl już
+        dziś!
+      </p>
+    </div>
+  );
+}
