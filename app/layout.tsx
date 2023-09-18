ed79db48b560d53a38dec1fd60987a472b2b430a
Kacper Wąsik <kacper@wasik.me>
2023-09-18 18:35:39 +0200

    refactor: mock data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,13 @@ import {Inter} from 'next/font/google';
 import Script from 'next/script';
 import {Footer} from 'ui/components/Layout/Footer';
 import {Header} from 'ui/components/Layout/Header';
-import {get_words_count} from 'ui/utils/word/get_words_count';
 
 const inter = Inter({subsets: ['latin']});
 
 export {metadata} from 'metadata';
 
 export default async function RootLayout({children}: {children: React.ReactNode}) {
-  const words_count = await get_words_count();
+  // const words_count = await get_words_count();
 
   return (
     <html lang='pl'>
@@ -23,7 +22,7 @@ export default async function RootLayout({children}: {children: React.ReactNode}
             <main className='border px-4 pb-4 pt-2 w-full sm:w-[75%] lg:w-[50%]'>{children}</main>
           </div>
 
-          <Footer words_count={words_count} />
+          <Footer words_count={5122} />
         </div>
 
         <Script src='https://www.googletagmanager.com/gtag/js?id=G-XJ2H501N3E' />
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,19 @@ import {HomePageContext} from 'ui/components/Pages/HomePageContext';
 import {Feedback} from 'ui/components/Word/Feedback';
 import {Word} from 'ui/components/Word/Word';
 import {WordDefinition} from 'ui/components/Word/WordDefinition';
-import {get_upvotes_count} from 'ui/utils/vote/get_upvotes_count';
-import {get_random_word} from 'ui/utils/word/get_random_word';
 
 export default async function Home() {
-  const {word, word_definition, id: word_id} = await get_random_word();
-  const upvotes = await get_upvotes_count(word_id);
+  // const {word, word_definition, id: word_id} = await get_random_word();
+  // const upvotes = await get_upvotes_count(word_id);
 
   return (
     <HomePageContext
       context={{
-        word,
-        word_definition,
-        word_id,
-        upvotes,
+        word: 'Lorem, ipsum dolor.',
+        word_definition:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam quos hic quo exercitationem, at porro ratione autem natus ipsa excepturi.',
+        word_id: 1,
+        upvotes: 212,
       }}
     >
       <Word />
